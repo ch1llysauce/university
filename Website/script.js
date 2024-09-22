5189677296d57c8cad85dccc0901c4b02ca4995d
Jkllen <150811694+Jkllen@users.noreply.github.com>
2024-09-23 00:15:18 +0800

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -3,6 +3,8 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+
 
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
@@ -19,4 +21,22 @@ btnPopup.addEventListener('click', ()=>{
 
 iconClose.addEventListener('click', ()=>{
     wrapper.classList.remove('active-popup');
+});
+
+
+dropdownToggle.addEventListener('click', (event) => {
+    event.preventDefault();
+    const dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.classList.toggle('show');
+});
+
+
+// Close the dropdown if the user clicks outside of it
+window.addEventListener('click', (event) => {
+    if (!event.target.matches('.dropdown-toggle')) {
+        const dropdowns = document.querySelectorAll('.dropdown-content');
+        dropdowns.forEach(dropdown => {
+            dropdown.classList.remove('show');
+        });
+    }
 });
