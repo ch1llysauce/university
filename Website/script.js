e7f118f38cb8d986d1a1acdce52dcadf375dc671
Jkllen <150811694+Jkllen@users.noreply.github.com>
2024-09-22 22:18:12 +0800

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -1,5 +1,15 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
+});
+
+
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
